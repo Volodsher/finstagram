bdf1526190414b14f215b3e9e127e49c4e086b23
Volodsher <Volodsher@gmail.com>
2022-02-03 00:06:00 +0000

    now with db

--- app/actions.rb
+++ app/actions.rb
@@ -17,7 +17,7 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -27,10 +27,14 @@ get '/' do
     comments: [{
       username: "sharky_j",
       text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+    },
+    {
+      username: "sharky_b",
+      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -43,7 +47,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -56,5 +60,9 @@ get '/' do
     }]
   }
 
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+  # [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+
+  erb(:index)
 end
