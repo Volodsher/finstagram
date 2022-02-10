78adc6e69f7ac67fc48038deb8e7b143d8ab0f21
Volodsher <Volodsher@gmail.com>
2022-02-10 10:21:34 +0000

    signup route

--- app/actions.rb
+++ app/actions.rb
@@ -71,3 +71,35 @@ get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
+
+post '/signup' do
+  # params.to_s
+  # grab user input values from params
+  email =      params[:email]
+  avatar_url = params[:avatar_url]
+  username =   params[:username]
+  password =   params[:password]
+  
+  # if email.present? && avatar_url.present? && username.present? && password.present?
+
+  # instantiate and save a User
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+  
+  # if user validations pass and user is saved
+  if @user.save
+    "User #{username} saved!"
+    # return readable representation of User object
+    # escape_html user.inspect
+
+  else
+    erb(:signup)
+    # display simple error message
+    # escape_html user.errors.full_messages
+  end
+end
+
+get '/signup' do  # if a user navigates to the path "/signup",
+  @user = User.new # setup empty @user object
+  erb(:signup) # render "app/views/signup.erb"
+end
+
